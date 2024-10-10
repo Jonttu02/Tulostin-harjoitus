26e7ae93a87e48ce80f5dad84bea2afc5550b63c
Joonas Antero Haapakorva <joonas.haapakorva@gmail.com>
2024-10-10 11:35:45 +0300

    Lisäsin tulostin-harjoituksen repositoryyn

---
+++ Tulostin-ohjelmointiharjoitus/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hei olen Tulostin-ohjelma");
+
+
+        
+
+        String tekijä = "Joonas";
+
+        
+
+        System.out.println("Ohjelman tekijä: " + tekijä);
+
+        double luku1 = 5;
+        double luku2 = 2;
+        double tulo;
+
+        tulo = luku1 - luku2;
+
+        System.out.println("Luku1 muuttujan arvo on " + luku1);
+
+        System.out.println("Luku2 muuttujan arvo on " + luku2);
+
+        System.out.println("5 - 2 = " + tulo);
+
+
+        
+    }
+}
